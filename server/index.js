59d18cdfd9eab844db6b79584af8b93be53cd253
lvertiz-at-858853655499 <lvertiz@bluepeople.com>
2022-05-26 14:17:59 -0500

    IDK

--- server/index.js
+++ server/index.js
@@ -18,44 +18,41 @@ function getUrl(params = {}){
 	return url
 }
 
-function init(){
+async function init(){
 	let Data = []
-	console.log('Getting data...')
-	axios.get(getUrl({count: 100}))
-		.then(res => {
-			Data = res.data
+	let res = await axios.get(getUrl({count: 100}))
+	Data = res.data
 
-			app.use(cors())
-			app.get('/:page', async (req, res) => {
-				let page = req.params.page
-				let startDate = req.query.startDate
-				let endDate = req.query.endDate
-				let search = req.query.search
+	app.use(cors())
+	app.get('/:page', async (req, res) => {
+		let page = req.params.page
+		let startDate = req.query.startDate
+		let endDate = req.query.endDate
+		let search = req.query.search
 
-				let items = Data
-				if(startDate){
-					items = items.filter(d => moment(startDate).isSameOrBefore(d.date))
-				}
-				if(endDate){
-					items = items.filter(d => moment(endDate).isSameOrAfter(d.date))
-				}
-				if(search){
-					items = items.filter(d => d.title.toLowerCase().includes(search.toLowerCase()))
-				}
-				let pages = Math.ceil(items.length/4)
-				items = items.slice((page-1)*4, page*4)
+		let items = Data
+		if(startDate){
+			items = items.filter(d => moment(startDate).isSameOrBefore(d.date))
+		}
+		if(endDate){
+			items = items.filter(d => moment(endDate).isSameOrAfter(d.date))
+		}
+		if(search){
+			items = items.filter(d => d.title.toLowerCase().includes(search.toLowerCase()))
+		}
+		let pages = Math.ceil(items.length/4)
+		items = items.slice((page-1)*4, page*4)
 
-				res.send({
-					items,
-					page,
-					pages,
-				})
-			})
-
-			app.listen(port, () => {
-				console.log(`App listening on port ${port}`)
-			})
+		res.send({
+			items,
+			page,
+			pages,
 		})
+	})
+
+	app.listen(port, () => {
+		console.log(`App listening on port ${port}`)
+	})
 }
 
 init()
